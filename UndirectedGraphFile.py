ecf60f4e98101034215954ecc6e919c266522824
harlanhowe <harlan.howe@kinkaid.org>
2023-10-28 22:00:15 -0500

    Repaired version from solution code

--- UndirectedGraphFile.py
+++ UndirectedGraphFile.py
@@ -1,24 +1,28 @@
 from DirectedGraphFile import DirectedGraph
-from typing import Dict, Tuple, List
+from typing import Dict, List
 from TypesAndConstants import *
+
 
 class UndirectedGraph(DirectedGraph):
 
-    def __init__(self, V: Dict[int, Vertex] = None,
-                       E: Dict[int, Edge] = None,
-                       filename: str = None,
-                       keys: Tuple[str] = ()) -> None:
-        super().__init__(V=V, E=E, filename=filename,keys=keys)
+    def __init__(self,
+                 V: Dict[int, Vertex] = None,
+                 E: Dict[int, Edge] = None,
+                 filename: str = None,
+                 keys: Tuple[str] = ()) -> None:
+        super().__init__(V=V, E=E, filename=filename, keys=keys)
         self.i_am_directed: bool = False
         self.EDGE_OFFSET: int = 0
 
+        self.edge_tables_dirty = True
+        self.edge_table: Dict[int, List[int]] = {}
 
     def generate_edge_tables(self) -> None:
         if self.edge_tables_dirty:
-            self.edge_table: Dict[int,List[int]] = {}
+            self.edge_table.clear()
             for e_id in self.E:
-                e:Edge = self.E[e_id]
-                u:int = e[KEY_U]
+                e: Edge = self.E[e_id]
+                u: int = e[KEY_U]
                 if u in self.edge_table:
                     self.edge_table[u].append(e_id)
                 else:
@@ -30,21 +34,25 @@ class UndirectedGraph(DirectedGraph):
                     self.edge_table[v] = [e_id]
             self.edge_tables_dirty = False
 
-    def get_edges_touching(self, vertex_id:int) -> List[Edge]:
+    def get_edges_touching(self, u_vertex_id: int) -> List[Edge]:
         """
-        :param u_index: the id number for a given vertex, u
-        :return: a list of edge id numbers for all edges that touch the vertex u. If there are no edges touching this vertex,
-                returns an empty list.
+        :param u_vertex_id: the id number for a given vertex, u
+        :return: a list of edge id numbers for all edges that touch the vertex u. If there are no edges touching this
+                vertex, returns an empty list.
         """
         if self.edge_tables_dirty:
             self.generate_edge_tables()
-        if vertex_id in self.edge_table:
-            return self.edge_table[vertex_id]
+        if u_vertex_id in self.edge_table:
+            edge_list: List[Edge] = []
+            for edge_id in self.edge_table[u_vertex_id]:
+                edge_list.append(self.E[edge_id])
+            return edge_list
         return []
 
-    def get_edges_from_u(self, u_id:int) -> List[Edge]:
+    def get_edges_from_u(self, u_id: int) -> List[Edge]:
         """ overrides directed version """
         return self.get_edges_touching(u_id)
+
     def get_edges_to_v(self, v_id: int) -> List[Edge]:
         """ overrides directed version """
         return self.get_edges_touching(v_id)
